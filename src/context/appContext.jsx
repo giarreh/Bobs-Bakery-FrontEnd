a2fe4bb0ff8d8812133fbdc41f37212bd0b08496
Giar Rehani <giarreh1@gmail.com>
2024-03-15 14:25:42 +0100

    Refactored name

--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -1,49 +1,38 @@
-import { createContext, useEffect, useState } from 'react'
+import { createContext, useEffect, useState } from 'react';
 
 export const AppContext = createContext();
 
-export default function AppContextProvider({children}) {
+export default function AppContextProvider({ children }) {
   const [posts, setPosts] = useState([]);
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  // placeholder for the get requests (POSTS)
-  useEffect(() => {
-
-    const fetchPostData = async () => {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-      const data = await response.json()
-      setPosts(data)
-      console.log("POSTS ARE SET: ", data)
-    } catch (error) {
-      throw new Error('Error fetching the posts')
-    }
-    }
-    fetchPostData()
-  }, [])
-
-  // placeholder for the get requests (USERS)
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        const data = await response.json()
-        setUsers(data)
-        setLoading(false)
-        console.log("USERS ARE SET: ", data)
-      } catch (error) {
-        throw new Error('Error fetching the users')
-      }
-    }
-    fetchData()
-  }, [])
+        const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users');
 
+        const postsData = await postsResponse.json();
+        const usersData = await usersResponse.json();
+
+        setPosts(postsData);
+        setUsers(usersData);
+        setLoading(false);
+
+        console.log("POSTS ARE SET: ", postsData);
+        console.log("USERS ARE SET: ", usersData);
+      } catch (error) {
+        throw new Error('Error fetching data');
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <AppContext.Provider value={{posts, users}}>
-      {!loading && children}
+    <AppContext.Provider value={{ posts, users }}>
+      {loading ? <p>Loading...</p> : children}
     </AppContext.Provider>
-  )
+  );
 }
