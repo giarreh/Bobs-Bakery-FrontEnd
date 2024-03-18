86fb23ebe8dfc6503d0b313cdf3a5cf2175b0e07
Giar Rehani <giarreh1@gmail.com>
2024-03-18 09:14:30 +0100

    Post list with dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import AppContextProvider from './Context/AppContext';
+import AppContextProvider from './context/AppContext';
 import MainBody from './components/MainBody';
 import Sidebar from './components/Sidebar';
 import Header from './components/header';
