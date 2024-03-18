d7d65ef07eb23e0d4b4f3d63cf1cc73783f1fa5f
Giar Rehani <giarreh1@gmail.com>
2024-03-18 10:52:52 +0100

    Styling for signup

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,11 +1,13 @@
-import { useContext, useState } from 'react'
+// Signup.js
+
+import { useContext, useState } from 'react';
 import { UserContext } from '../../context/UserContext';
 import { useNavigate } from 'react-router-dom';
+import './Signup.css';
 
 export default function Signup() {
-  const {user, setUser} = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
   const navigate = useNavigate();
-
 
   const [form, setForm] = useState({
     username: '',
@@ -16,56 +18,55 @@ export default function Signup() {
     favoriteColor: '',
   });
 
-
   const handleSubmit = (e) => {
     e.preventDefault();
     setUser(form);
-    console.log('User signed up: ', form)
+    console.log('User signed up: ', form);
     navigate('/posts');
-  }
+  };
 
   return (
-    <div>
-      <h1>Signup</h1>
-      <form>
-        <input 
-          type="text" 
-          placeholder="Username" 
-          value={form.username} 
-          onChange={(e) => setForm({...form, username: e.target.value})} 
+    <div className="signup-container">
+      <h1>Register a new user</h1>
+      <form className="signup-form">
+        <input
+          type="text"
+          placeholder="Username"
+          value={form.username}
+          onChange={(e) => setForm({ ...form, username: e.target.value })}
         />
-        <input 
-          type="password" 
-          placeholder="Password" 
-          value={form.password} 
-          onChange={(e) => setForm({...form, password: e.target.value})} 
+        <input
+          type="password"
+          placeholder="Password"
+          value={form.password}
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
         />
-        <input 
-          type="text" 
-          placeholder="First Name" 
-          value={form.firstName} 
-          onChange={(e) => setForm({...form, firstName: e.target.value})} 
+        <input
+          type="text"
+          placeholder="First Name"
+          value={form.firstName}
+          onChange={(e) => setForm({ ...form, firstName: e.target.value })}
         />
-        <input 
-          type="text" 
-          placeholder="Last Name" 
-          value={form.lastName} 
-          onChange={(e) => setForm({...form, lastName: e.target.value})} 
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={form.lastName}
+          onChange={(e) => setForm({ ...form, lastName: e.target.value })}
         />
-        <input 
-          type="email" 
-          placeholder="Email" 
-          value={form.email} 
-          onChange={(e) => setForm({...form, email: e.target.value})} 
+        <input
+          type="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
         />
-        <input 
-          type="text" 
-          placeholder="Favorite Color" 
-          value={form.favoriteColor} 
-          onChange={(e) => setForm({...form, favoriteColor: e.target.value})} 
+        <input
+          type="text"
+          placeholder="Favorite Color"
+          value={form.favoriteColor}
+          onChange={(e) => setForm({ ...form, favoriteColor: e.target.value })}
         />
-        <button onClick={handleSubmit}>Signup</button>
+        <button onClick={handleSubmit}>Sign up</button>
       </form>
     </div>
-  )
+  );
 }
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -16,7 +16,6 @@ export default function UserContextProvider({children}) {
   //     });
   // }, []
   // );
-
   
 
   return (
