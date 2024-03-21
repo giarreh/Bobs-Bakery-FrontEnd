1926a554826d24cba19607d8980a456d834f00ac
Giar Rehani <giarreh1@gmail.com>
2024-03-21 13:51:08 +0100

    Newest posts are showed first

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -3,8 +3,8 @@ import { AppContext } from '../../context/AppContext';
 import PostListItem from './PostListItem';
 export default function PostList() {
   const { posts } = useContext(AppContext);
-  //const limitedPosts = posts.slice(posts.length - 5, posts.length);
-  const limitedPosts = posts.slice(0, 10);
+  const limitedPosts = posts.slice(posts.length - 5, posts.length);
+  //const limitedPosts = posts.slice(0, 10);
 
   return (
     <div>
